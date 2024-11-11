92847e5ceecfca0bf1d443900ca16ef37872389d
Penang Teams <raj_ppd@yahoo.com>
2024-11-12 00:19:19 +0800

    refine whitespace zod

--- app/lib/zodSchema.ts
+++ app/lib/zodSchema.ts
@@ -5,53 +5,74 @@ import { z } from "zod";
 //https://stackoverflow.com/questions/77134910/how-can-i-remove-all-whitespace-in-zod
 //https://stackoverflow.com/questions/77594561/how-to-remove-whitespace-from-within-a-string-using-zod
 export const loginSchema = z.object({
+  // username: z
+  //   .string()
+  //   .regex(/^[0-9]+$/, { message: "only numbers, " })
+  //   .max(4, { message: "4 digits" })
+  //   .min(4, { message: "4 digits" }),
   username: z
     .string()
-    .trim()
-    .transform((s, ctx) => {
-      const withoutWhitespace = s.replaceAll(/\s*/g, "");
-      if (withoutWhitespace.length !== 4) {
-        //can for 4d, 3d, 2d
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "You must enter 4 digits",
-        });
-        return z.NEVER;
-      }
-      return withoutWhitespace;
-    }),
+    .refine((s) => !s.includes(" "), " no spaces!")
+    .pipe(
+      z
+        .string()
+        .min(5, { message: "Username must more than 4 characters," })
+        .max(10, { message: "Username must less than 11 characters," })
+        .regex(/^[^\W_].*[^\W_]$/, {
+          message: " not use special characters at start or end",
+        })
+    ),
+
   password: z
     .string()
-    .min(4, { message: "Password must more than 3 characters" })
-    .max(8, { message: "Password must less than 9 characters" }),
+    .refine((s) => !s.includes(" "), " no spaces!")
+    .pipe(
+      z
+        .string()
+        .min(4, { message: "Password must more than 3 characters" })
+        .max(8, { message: "Password must less than 9 characters" })
+    ),
 });
 
 export const registerSchema = z.object({
   email: z.string().email(),
-  // username: z
-  //   .string()
-  //   .min(5, { message: "Username must more than 4 characters" })
-  //   .max(8, { message: "Username must less than 9 characters" }),
-  // does not count whitespace
-  // username: z.coerce.number().int().lte(9999),
   username: z
     .string()
-    .trim()
-    .transform((s, ctx) => {
-      const withoutWhitespace = s.replaceAll(/\s*/g, "");
-      if (withoutWhitespace.length !== 4) {
-        //can for 4d, 3d, 2d
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "You must enter 4 digits",
-        });
-        return z.NEVER;
-      }
-      return withoutWhitespace;
-    }),
+    .refine((s) => !s.includes(" "), " no spaces!")
+    .pipe(
+      z
+        .string()
+        .min(5, { message: "Username must more than 4 characters," })
+        .max(10, { message: "Username must less than 11 characters," })
+        .regex(/^[^\W_].*[^\W_]$/, {
+          message: " not use special characters at start or end",
+        })
+    ),
+
+  // does not count whitespace
+  // username: z.coerce.number().int().lte(9999),
+  // username: z
+  //   .string()
+  //   .trim()
+  //   .transform((s, ctx) => {
+  //     const withoutWhitespace = s.replaceAll(/\s*/g, "");
+  //     if (withoutWhitespace.length <= 3) {
+  //       ctx.addIssue({
+  //         code: z.ZodIssueCode.custom,
+  //         message: "You must enter minimum 4 characters",
+  //       });
+  //       return z.NEVER;
+  //     }
+  //     return withoutWhitespace;
+  //   }),
 
   password: z
     .string()
-    .min(4, { message: "Password must more than 3 characters" })
-    .max(8, { message: "Password must less than 9 characters" }),
+    .refine((s) => !s.includes(" "), " no spaces!")
+    .pipe(
+      z
+        .string()
+        .min(4, { message: "Password must more than 3 characters" })
+        .max(8, { message: "Password must less than 9 characters" })
+    ),
 });
